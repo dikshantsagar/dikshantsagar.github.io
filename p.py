24cff9f12cfb422331156069eb4301ebe3cb4f75
Dikshant Sagar <sagar.dikshant@gmail.com>
2021-03-11 19:21:42 +0530

    Update p.py

--- p.py
+++ p.py
@@ -256,10 +256,10 @@ class ROIPooler(nn.Module):
 			mask = torch.logical_and((boxes[:,3]-boxes[:,1]) > 1,(boxes[:,4]-boxes[:,2]) > 1)
 			height = boxes[:,4] - boxes[:,2] + 1
 			width = boxes[:,3] - boxes[:,1] + 1
-            		if boxes.shape > 0:
+			if boxes.shape > 0 :
 				max_h,max_w = torch.max(torch.max(height),0)[0], torch.max(torch.max(width),0)[0]
 				max_h,max_w = torch.max(torch.tensor([max_h,3])), torch.max(torch.tensor([max_w,3]))
-            		else:
+			else:
 				max_h,max_w = torch.tensor(1,device=device),torch.tensor(1,device=device)
 			output = torch.zeros(
 				(num_boxes, num_channels, max_h, max_w), dtype=dtype, device=device
@@ -285,7 +285,7 @@ class ROIPooler(nn.Module):
 		elif(self.output_size == 7):
 
 			output = torch.zeros(
-            (num_boxes, num_channels, output_size, output_size), dtype=dtype, device=device)
+			(num_boxes, num_channels, output_size, output_size), dtype=dtype, device=device)
 
 			for level, pooler in enumerate(self.level_poolers):
 				inds = nonzero_tuple(level_assignments == level)[0]
